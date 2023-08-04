a2856d8818c7d38260789dbf32d48d76ca45253e
Howard <howard.tzw@gmail.com>
2023-08-04 15:13:02 +0800

    Update icons

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,20 +1,19 @@
 import { createApp, h } from 'vue'
 import VueSelect from '@/index'
 import App from './app.vue' // must be imported after VueSelect
-import IconDown from './components/IconDown.vue'
 import router from './router'
-
+// import IconDown from './components/IconDown.vue'
 import '@/css/vue-select.css'
 import './style.css'
 
-VueSelect.props.components.default = () => ({
-  // Deselect: {
-  // 	render: () => h('span', '❌'),
-  // },
-  OpenIndicator: {
-    render: () => h(IconDown),
-  },
-})
+// VueSelect.props.components.default = () => ({
+//   Deselect: {
+//   	render: () => h('span', '❌'),
+//   },
+//   OpenIndicator: {
+//     render: () => h(IconDown),
+//   },
+// })
 
 const app = createApp(App)
 app.use(router)
