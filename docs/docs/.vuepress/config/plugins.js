987b466a2b703edc4ab18bbca50e35a49b08f432
Alexander Vorobyev <voral.adm@gmail.com>
2023-12-14 15:06:59 +0300

    docs: sitemap

--- docs/docs/.vuepress/config/plugins.js
+++ docs/docs/.vuepress/config/plugins.js
@@ -2,6 +2,7 @@ import {registerComponentsPlugin} from '@vuepress/plugin-register-components';
 import {activeHeaderLinksPlugin} from '@vuepress/plugin-active-header-links';
 import {searchPlugin} from '@vuepress/plugin-search';
 import yandexMetrikaPlugin from 'vuepress-plugin-yandex-metrika';
+import sitemapPlugin from '@vuepress-denaro/vuepress-plugin-sitemap';
 
 export default [
     registerComponentsPlugin({
@@ -17,5 +18,9 @@ export default [
             accurateTrackBounce: true,
             ecommerce: "dataLayer"
         }
+    }),
+    sitemapPlugin({
+        hostname: 'https://vue3-select.va-soft.ru/',
+        exclude: ['/404.html', 'sandbox.html']
     }),
 ]
