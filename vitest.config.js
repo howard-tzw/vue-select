aa494819ebbfe7cc624eaf7a5b698ba5f15ef8b6
Alexander Vorobyev <voral.adm@gmail.com>
2023-12-15 17:03:12 +0300

    test(config): Configure coverage exclude

--- vitest.config.js
+++ vitest.config.js
@@ -1,23 +1,16 @@
-import {defineConfig, mergeConfig, configDefaults} from 'vitest/config'
-import viteConfig from './vite.config'
+import {defineConfig, configDefaults} from 'vitest/config'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
-
-// import { defineConfig, mergeConfig } from 'vitest/config'
-// import viteConfig from './vite.config'
-//
-// export default mergeConfig(viteConfig, defineConfig({
-//     test: {
-//         exclude: ['demo/*'],
-//     },
-// }))
 
 export default defineConfig({
     plugins: [vue()],
     test: {
-        exclude: [...configDefaults.exclude, '**/demo/src/**','demo/**','**/demo/**',path.resolve(__dirname, './demo/')+'/**','demo'],
+        environment: 'jsdom',
+        coverage: {
+            provider: 'v8',
+            exclude: [...configDefaults.coverage.exclude, 'demo'],
+        },
     },
-    exclude: [...configDefaults.exclude, '**/demo/src/**','demo/**','**/demo/**','demo'],
     resolve: {
         alias: {
             '#': path.resolve(__dirname, './tests'),
@@ -25,5 +18,3 @@ export default defineConfig({
         },
     },
 })
-//
-console.log([...configDefaults.exclude, '**/demo/src/**','demo/**','**/demo/**',path.resolve(__dirname, './demo/')+'/**','demo'])
